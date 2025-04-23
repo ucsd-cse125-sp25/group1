70985af356564e7d090365120b57ae0472fdb505
miyuki-l <59126064+Miyuki-L@users.noreply.github.com>
2025-04-23 14:57:01 -0700

    added doc string to client.hpp

--- include/client/client.hpp
+++ include/client/client.hpp
@@ -15,26 +15,109 @@
 
 class Client {
 public:
+    /**
+     *  @brief Constructs the Client object and initialize TCP  
+     */
     Client();
+
+    /**
+     *  @brief Destructor for the Client object
+     */
     ~Client();
 
+    /**
+     *  @brief Verifies initializations are setup
+     * 
+     * Verifies connection to server 
+     * 
+     * @return true if initialized correctly, false otherwise
+     */
     bool init();
+
+    /**
+     *  @brief Main Execution method for Client 
+     * 
+     * initializes window, GL, Scence
+     */
     void run();
 
+    /**
+     *  @brief Camera object representing player's cur position
+     */
     Camera camera;
 
 private:
+    /**
+     *  @brief Sets up the connection with server
+     * 
+     * Connects to server, sets ClientId to Id received from server
+     * 
+     * @return true, if successfully connected to server, false otherwise
+     */
     bool connectToServer();
 
+    /**
+     *  @brief Receive and extracts message from server and pass to handleServerMessage
+     * 
+     * Ensures valid Socket and attempts to read message from server and extract message
+     * 
+     */
     void receiveServerMessage();
+
+    /**
+     *  @brief Handles the Message received from the Server
+     * 
+     * Based on the "type" value of the messaged received from the server, 
+     *  resolve the server message accordingly
+     *
+     * @param message A JSON string containing updates from the server
+     */
     void handleServerMessage(std::string message);
 
+    /**
+     *  @brief Watch for Player inputs, and if sends input to   
+     * 
+     *  Keys checked:
+     * - W / Up Arrow --> move_forward
+     * - S / Down Arrow  --> move_backward
+     * - A / Left Arrow  --> strafe_left
+     * - D / Right Arrow --> strafe_right
+     * 
+     * sends JSON string packet of { type: "input", actions : [array of acctions] } to server
+     * 
+     *  @param window Pointer to the active GLFW window used to query key states.
+     */
     void handlePlayerInput(GLFWwindow* window);
+
+    /**
+     *  @brief Creates the Packed to send the provided message to the server
+     *
+     *  @param message A JSON string containing updates to send to server
+     */
     void sendMessageToServer(const nlohmann::json& message);
 
+    /**
+     *  @brief Initializes GLFW Window
+     * 
+     *  @return true, if GLFW window successfully initialized, false otherwise
+     */
     bool initWindow(GLFWwindow*& window);
+
+    /**
+     *  @brief Initializes GL
+     */
     void initGL();
+
+    /**
+     *  @brief Initializes the Scene
+     * 
+     * Initialize the scence with camera at client's position
+     */
     void initScene();
+
+    /**
+     *  @brief Main game rendering and update loop for 
+     */
     void gameLoop(GLFWwindow* window);
     void cleanup(GLFWwindow* window);
 

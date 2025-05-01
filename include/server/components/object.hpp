adb795664e6c32623a77bf3f42b19eedc964f584
rshende25 <rshende@ucsd.edu>
2025-05-01 09:04:05 -0700

    added object class to server

--- include/server/components/object.hpp
+++ include/server/components/object.hpp
@@ -1,10 +1,29 @@
 #pragma once
+#include <glm/glm.hpp>
+#include "rigidBody.hpp"
 
 class Object
 {
 public:
-    Object(/*TODO: add rigidbody*/);
-    ~Object();
+    /**
+     * @brief Constructs an object with a given id, room ID, position, and direction.
+     *
+     * Initializes an object at the specified position and orientation,
+     * assigning it a box collider based on the given width and height.
+     * An object is any item in a room that is not interactable via a key press.
+     * It only has a rigidbody that can be collided with.
+     *
+     * @param id ID of the object
+     * @param position Initial position of the player.
+     * @param direction Initial facing direction of the player.
+     * @param width width of the object.
+     * @param height height of the object.
+     */
+    Object(int id, glm::vec3 position, glm::vec3 direction, float width, float height);
+
+    ~Object() = default;
+    
 private:
-    // TODO: add graphics
+    int id;
+    RigidBody body;
 };
--- src/server/components/object.cpp
+++ src/server/components/object.cpp
@@ -1,1 +1,16 @@
 #include "server/components/object.hpp"
+
+Object::Object(int id, glm::vec3 position, glm::vec3 direction, float width, float height)
+    : 
+    id(id),
+    body(
+        vec3(0.0f),
+        vec3(0.0f),
+        0.0f, // mass of 0 - kinematics doesn't matter because it's imovable
+        new Transform { position, direction },
+        new BoxCollider{
+            AABB,
+            vec3(-width / 2, -height / 2, -width / 2),
+            vec3(width / 2, height / 2, width / 2)
+        }
+    ) {}

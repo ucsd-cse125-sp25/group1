02689e3c350efad789db332f190a6e6fa2f2e1f7
breponte <breponte03@gmail.com>
2025-05-16 11:48:08 -0700

    Adjusted inheritance of classes

--- include/server/components/interactable.hpp
+++ include/server/components/interactable.hpp
@@ -3,14 +3,19 @@
 #include "components/object.hpp"
 
 class Player;
+class Object;
 
 class Interactable : public Object
 {
 public:
-    Interactable();
+    Interactable(int id, const glm::vec3& position, const glm::vec3& direction, float width, float height);
     virtual ~Interactable() = default;
     void interact(const Player &player); //calls the overriden handleInteract function.
 
 protected:
     virtual void handleInteract(const Player &player) = 0; //Pure virtual function, must be implemented by derived classes
+
+private:
+    int objectID;
+    RigidBody body;
 };
